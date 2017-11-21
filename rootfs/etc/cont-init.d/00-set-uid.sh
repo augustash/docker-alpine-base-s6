c2bc5b721a9f9b86f9e4b1b4a7e98c5f839ffb81
Pete McWilliams <petemcw@gmail.com>
2017-11-21 13:36:06 -0600

    Sets execution mode and silences any errors.
    The `ash` doesn't have a home directory and when changing the UID a
    non-helpful error can trigger.

--- rootfs/etc/cont-init.d/00-set-uid.sh
+++ rootfs/etc/cont-init.d/00-set-uid.sh
@@ -3,7 +3,7 @@
 # default to MacOS user
 PUID=${PUID:-501}
 PGID=${PGID:-1000}
-if [[ ! "$(id -g ash)" -eq "$PGID" ]]; then groupmod -o -g "$PGID" ash; fi
-if [[ ! "$(id -u ash)" -eq "$PUID" ]]; then usermod -o -u "$PUID" ash; fi
+if [[ ! "$(id -g ash)" -eq "$PGID" ]]; then groupmod -o -g "$PGID" ash &>/dev/null; fi
+if [[ ! "$(id -u ash)" -eq "$PUID" ]]; then usermod -o -u "$PUID" ash &>/dev/null; fi
 
 echo "============> User ID: $(id -u ash) / Group ID: $(id -g ash)"
